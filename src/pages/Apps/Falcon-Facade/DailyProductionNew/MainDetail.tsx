79a227cfd248b9c03fe580bb92c9692b169e060d
ravirajladha <ravirajladha@gmail.com>
2025-03-07 17:58:30 +0530

    breadcrumb fix in the facade

--- src/pages/Apps/Falcon-Facade/DailyProductionNew/MainDetail.tsx
+++ src/pages/Apps/Falcon-Facade/DailyProductionNew/MainDetail.tsx
@@ -313,8 +313,8 @@ const WorkOrderPage = () => {
 
     const breadcrumbItems = [
         { label: 'Home', link: '/', isActive: false },
-        { label: 'Konkrete Klinkers', link: '/', isActive: false },
-        { label: 'Work Order', link: '/konkrete-klinkers/work-order/view', isActive: false },
+        { label: 'Falcon Facade', link: '/', isActive: false },
+        { label: 'Work Order', link: '/falcon-facade/work-order/view', isActive: false },
         { label: 'Detail Page', link: '#', isActive: true },
     ];
 
@@ -323,7 +323,7 @@ const WorkOrderPage = () => {
         <div>
             <Breadcrumbs
                 items={breadcrumbItems}
-                addButton={{ label: 'Back', link: '/konkrete-klinkers/work-order/view', icon: <IconArrowBackward className="text-4xl" /> }}
+                addButton={{ label: 'Back', link: '/falcon-facade/work-order/view', icon: <IconArrowBackward className="text-4xl" /> }}
             />
             <button
                 onClick={() => window.print()}
